f65e6766b50939ec9cfe724faf8c7547b9dd1d9c
dcamilobj <dcamilobj@gmail.com>
2017-10-14 09:05:58 -0500

    Changed database route

--- app/models/Room.scala
+++ app/models/Room.scala
@@ -7,17 +7,17 @@ import org.mongodb.scala.bson.ObjectId
 import org.mongodb.scala.bson.codecs.DEFAULT_CODEC_REGISTRY
 import play.api.libs.json.{JsString, JsValue, Json, Writes}
 
-case class Room(_id:ObjectId, room_id: Double, hotel_id: Double, room_type:String,
-                city:String, capacity: Double, price:Double, currency:String,
+case class Room(_id:ObjectId, room_id: Int, hotel_id: Int, room_type:String,
+                city:String, capacity: Int, price:Int, currency:String,
                 description:String)
 
 object Room{
 
-  def apply( room_id: Double, hotel_id: Double, room_type:String,
-             city:String,capacity: Double, price:Double, currency:String,
+  def apply( room_id: Int, hotel_id: Int, room_type:String,
+             city:String, capacity: Int, price:Int, currency:String,
              description:String): Room =
-    Room(new ObjectId(),room_id: Double, hotel_id: Double, room_type:String,
-      city:String,capacity: Double, price:Double, currency:String,
+    Room(new ObjectId(),room_id: Int, hotel_id: Int, room_type:String,
+      city:String, capacity: Int, price:Int, currency:String,
       description:String)
 
   //Para poder convertir la colección en BSON y viceversa
--- build.sbt
+++ build.sbt
@@ -10,6 +10,8 @@ scalaVersion := "2.12.3"
 libraryDependencies += guice
 libraryDependencies  += "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.0" % Test
 libraryDependencies += "org.mongodb.scala" %% "mongo-scala-driver" % "2.1.0"
+
+herokuAppName in Compile := "dezameron-dae"
 // Adds additional packages into Twirl
 //TwirlKeys.templateImports += "co.edu.udea.dae.controllers._"
 
